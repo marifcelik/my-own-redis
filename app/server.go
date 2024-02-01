9664abc76e50257426c989d790b7188a1262866b
arif <arif-celik56@hotmail.com>
2024-02-01 21:37:09 +0300

    1st stage

--- app/server.go
+++ app/server.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"log"
+	"net"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp4", "0.0.0.0:6379")
+	if err != nil {
+		log.Fatal("Failed to bind to port 6379")
+	}
+	_, err = l.Accept()
+	if err != nil {
+		log.Fatal("Error accepting connection: ", err.Error())
+	}
 }
